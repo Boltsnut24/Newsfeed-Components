807b455691ca2388124045967038fa4c825cc1c8
Brad <Brad@BRADSPC>
2019-06-19 14:49:24 -0600

    Added menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,20 @@
+class Menu {
+  constructor(menu){
+    this.menuOpen = menu.querySelector('.menu');
+    this.menuButton = menu.querySelector('.menu-button');
+    this.menuButton.addEventListener('click', () => this.toggleMenu());
+  }
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  toggleMenu(){
+    this.menu.classList.toggleAttribute('toggle-on');
+    // Toggle the "menu--open" class on your menu refence. 
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menus = document.querySelectorAll('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menus.forEach(menu => new Menu (menu));
